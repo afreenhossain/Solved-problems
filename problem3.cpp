f8aeed762334d44886b54d1df6ef7f34287384fd
Afreen Hossain <barsha052@gmail.com>
2015-12-02 23:06:17 +1100

    Update problem3.cpp

--- problem3.cpp
+++ problem3.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 
 // Largest prime factor
 bool isPrime(unsigned long long n){
 bool prime = true;
-    for (int j = 2; j <= n/2; j++){
+    for (int j = 2; j < sqrt(n); j++){
         if(n%j==0){
             prime = false;
             break;
@@ -16,7 +17,7 @@ return prime;
 int main (){
 unsigned long long number = 600851475143ULL;
 
-for(unsigned long long i = number/2; i >= 2; i--){
+for(unsigned long long i = sqrt(number); i >= 2; i--){
     if(number%i==0){
         if(isPrime(i)){
             cout << i;
